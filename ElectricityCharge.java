6eaf85260cd0d6d7d4267aec74ed3dc34d40a8c2
manju771 <124677507+manju771@users.noreply.github.com>
2023-07-11 19:03:21 +0530

    Update ElectricityCharge.java

--- ElectricityCharge.java
+++ ElectricityCharge.java
@@ -8,6 +8,19 @@ public class ElectricityCharge{
 
     System.out.println("Enter the electricity ")
       float ele=sc.nextFloat();
+    double amount=0;
+    double TotalAmount=0;
+    if(unit<50){
+      amount=unit*0.5;
+    }else if(unit<=150){
+      amount=50*0.5+(unit-50)*0.75;
+    }else if(unit<=250){
+      amount=50*0.5+100*0.75+(unit-150)*1.2;
+    }else{
+      amount=50*0.5+100*0.75+100*1.2+(unit-250)*1.5;
+    }
+    TotalAmount=(amount+amount*0.2);
+    System.out.println("total bill is"+TotalAmount);
 
     
   }
